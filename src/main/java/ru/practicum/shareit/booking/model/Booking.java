53dc37f4e1320945b4cb47f80c2f407822813b3b
Da4nie-persick <darya.tcheltygmasheva@yandex.ru>
2023-07-23 14:20:47 +0700

    тз14

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
